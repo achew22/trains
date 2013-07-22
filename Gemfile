6bf80264db43f8cf9fc4a8694a70ddd000617558
Andrew Allen <achew22@achew22.com>
2013-07-22 00:04:15 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem "rake"
+
+group :test, :development do
+  gem "rspec"
+  gem "guard"
+  gem "guard-rspec"
+end
---
+++ spec/trains_spec.rb
@@ -0,0 +1,45 @@
+describe "Trains" do
+  context "in Kiwiland" do
+    it "should measure the distance of the route A-B-C" do
+      pending
+    end
+
+    it "should measure the distance of the route A-D" do
+      pending
+    end
+
+    it "should measure the distance of the route A-D-C" do
+      pending
+    end
+
+    it "should measure the distance of the route A-E-B-C-D" do
+      pending
+    end
+
+    it "should measure the distance of the route A-E-D" do
+      pending
+    end
+
+    it "should measure the number of trips starting at C and ending at C with a maximum of 3 stops.  In the sample data below, there are two such trips: C-D-C (2 stops). and C-E-B-C (3 stops)" do
+      pending
+    end
+
+    it "should measure the number of trips starting at A and ending at C with exactly 4 stops.  In the sample data below, there are three such trips: A to C (via B,C,D); A to C (via D,C,D); and A to C (via D,E,B)" do
+      pending
+    end
+
+    it "should measure the length of the shortest route (in terms of distance to travel) from A to C" do
+      pending
+    end
+
+    it "should measure the length of the shortest route (in terms of distance to travel) from B to B" do
+      pending
+    end
+
+
+    it "should measure the number of different routes from C to C with a distance of less than 30.  In the sample data, the trips are: CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC" do
+      pending
+    end
+
+  end
+end
